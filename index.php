2b4d012f8d381c319e4fc19f354a631f2b4131d2
Braijon Glover <26751990+Sugarhead71099@users.noreply.github.com>
2017-10-31 20:28:39 -0500

    Update index.php
    Integrated API from openweathermap.org for info - curl integration is next

--- index.php
+++ index.php
@@ -3,16 +3,24 @@
 	$weather = "";
 	$error = "";
 	if(!empty($_GET)) {
-		$city = str_replace(" ", "", $_GET["city"]);
-        $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-      	$error = $file_headers[0] == "HTTP/1.1 404 Not Found" ? $file_headers[0] : "";
+		$city = $_GET['city'];
+		// $city = str_replace(" ", "", $_GET["city"]);
+       //  $file_headers = @get_headers("http://www.weather-forecast.com/locations/{$city}/forecasts/latest");
+      	// $error = $file_headers[0] == "HTTP/1.1 404 Not Found" ? $file_headers[0] : "";
 
-        if(!$error) {
-			$pageDisplay = file_get_contents("http://www.weather-forecast.com/locations/$city/forecasts/latest");
-			$topHalfPageDisplay = explode('1 &ndash; 3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $pageDisplay);
-			$bottomHalfPageDisplay = explode('</span></span></span></p><div class="forecast-cont"><div class="units-cont"><a class="units metric active">', $topHalfPageDisplay[1]);
-			$weather = $bottomHalfPageDisplay[0];
-		}
+        // if(!$error) {
+			$pageDisplay = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=' . urlencode($city) . ',uk&appid=140b66f59c18c14d1a68072437e05c17');
+			$weatherInfo = json_decode($pageDisplay, true);
+
+			if($weatherInfo['cod'] == 200) {
+				$temperatureInFahrenheit = intval($weatherInfo['main']['temp'] * 9 / 5 - 459.67);
+				$weather = "The weather in " . ucfirst($city) . " is currenty " . $weatherInfo['weather'][0]['description'] . ". The temperature is {$temperatureInFahrenheit}&deg;F and the wind speed is " . $weatherInfo['wind']['speed'] . "m/s.";
+			} else
+				$error = 'Could not find city - Please try again.';
+			// $topHalfPageDisplay = explode('1 &ndash; 3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $pageDisplay);
+			// $bottomHalfPageDisplay = explode('</span></span></span></p><div class="forecast-cont"><div class="units-cont"><a class="units metric active">', $topHalfPageDisplay[1]);
+			// $weather = $bottomHalfPageDisplay[0];
+		// }
 	}
 
 ?>
@@ -24,7 +32,23 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-		<link rel="stylesheet" href="style.css">
+		<style>
+			body, html {
+			  background: url(background.jpeg) no-repeat center center fixed;
+			  -webkit-background-size: cover;
+			  -moz-background-size: cover;
+			  -o-background-size: cover;
+			  background-size: cover;
+			}
+			
+			#weatherContainer {
+				margin-top: 25vh;
+			}
+			
+			#weatherDisplay {
+				margin-top: 5vh;
+			}
+		</style>
 		<title>Weather Scraper</title>
 	</head>
 	<body>
